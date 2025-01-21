2422c25db923358974e84c7b26e5d446dc9e8bea
giuliannachireno <giuliannachireno@gmail.com>
2025-01-21 18:53:39 +0100

    final session commit

--- While_example2.py
+++ While_example2.py
@@ -1,17 +1,17 @@
 #Virtual dice game, you win if you roll a 6, you lose 1 life if not
 #you have 3 lives
-
 from random import randint
 
 lives = 3
-while lives > 0:
+while True:
         diceroll = randint(1,6)
         if diceroll == 6:
-            print("Congrats! You win!")
+            print("Congrats! You rolled a 6, you win!")
             break
         else:
             lives = lives - 1 #you lose a life
             print(f"You rolled a {diceroll}. You lose a life, lives left: {lives}")
-
-else:
-            print("You lose!")
+            if lives == 0:
+                print("You lost!")
+                break
+            print("Thanks for playing my game")
